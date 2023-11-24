9e1c102985291c518e6f3380cab1d0c30eb3f3cd
AK Rasel <raselak101010@gmail.com>
2023-11-25 00:16:32 +0600

    OurFeatures add

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
     return (
         <div>
             <Banner/>
-            {/* <OurFeatures/>
-            <WhyShipEase/> */}
+            <OurFeatures/>
+            {/* <WhyShipEase/> */}
         </div>
     );
 };
