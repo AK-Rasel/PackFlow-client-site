1c435b731c7b333d460e8ff90651f8f057c7f6f7
AK Rasel <raselak101010@gmail.com>
2023-11-27 15:24:07 +0600

    Delivery List only  desigen

--- src/Dashboard/deliveryMan/DeliveryList.jsx
+++ src/Dashboard/deliveryMan/DeliveryList.jsx
@@ -1,50 +1,50 @@
 
 
-// const DeliveryList = () => {
-//     return (
-//         <div className="lg:min-h-screen">
-//             <div className="text-center font-extrabold text-5xl my-8">
-//                 <h1>All Book : 0</h1>
-//             </div>
-//             <div className="overflow-x-auto">
-//                 <table className="table table-xs table-pin-rows table-pin-cols">
-//                     <thead>
-//                         <tr className="text-sm -z-10  text-[#222427]">
-//                             <th></th>
-//                             <td>Booked User’s Name</td>
-//                             <td>Receivers Name</td>
-//                             <td>Booked User’s Phone</td>
-//                             <td>Recievers phone number</td>
-//                             <th>Receivers Addresse</th>
-//                             <th>Action</th>
-//                         </tr>
-//                     </thead>
-//                     <tbody>
+const DeliveryList = () => {
+    return (
+        <div className="lg:min-h-screen">
+            <div className="text-center font-extrabold text-5xl my-8">
+                <h1>All Book : 0</h1>
+            </div>
+            <div className="overflow-x-auto">
+                <table className="table table-xs table-pin-rows table-pin-cols">
+                    <thead>
+                        <tr className="text-sm -z-10  text-[#222427]">
+                            <th></th>
+                            <td>Booked User’s Name</td>
+                            <td>Receivers Name</td>
+                            <td>Booked User’s Phone</td>
+                            <td>Recievers phone number</td>
+                            <th>Receivers Addresse</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
                       
                         
 
-//                         <tr >
-//                                 <th>1</th>
-//                                 <td>DeliveryMen</td>
-//                                 <td>phone</td>
-//                                 <td>id</td>
-//                                 <td>product</td>
-//                                 <td>Receivers Addresse</td>
+                        <tr >
+                                <th>1</th>
+                                <td>DeliveryMen</td>
+                                <td>phone</td>
+                                <td>id</td>
+                                <td>product</td>
+                                <td>Receivers Addresse</td>
                                
-//                                 <th>
-//                                     <button className="btn mb-2 lg:mb-0 lg:mr-4">Cancel </button>
-//                                     <button className="btn">Deliver </button>
-//                                 </th>
-//                             </tr>
+                                <th>
+                                    <button className="btn mb-2 lg:mb-0 lg:mr-4">Cancel </button>
+                                    <button className="btn">Deliver </button>
+                                </th>
+                            </tr>
 
 
-//                     </tbody>
+                    </tbody>
 
-//                 </table>
-//             </div>
+                </table>
+            </div>
 
-//         </div>
-//     );
-// };
+        </div>
+    );
+};
 
-// export default DeliveryList;
+export default DeliveryList;
