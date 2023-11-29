9c68af1c07e4d2d343b6c7fff8e8f8579820eaf6
AK Rasel <raselak101010@gmail.com>
2023-11-29 22:29:17 +0600

    pagination done

--- src/Dashboard/Admin/AllUser.jsx
+++ src/Dashboard/Admin/AllUser.jsx
@@ -1,19 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../Hook/useAxiosSecure";
 import Swal from "sweetalert2";
-
+import { useState } from "react";
 
 
 const AllUser = () => {
     const axiosSecure = useAxiosSecure()
-    // const [page, setPage] = useState(0)
+
+
     const { data: allUsers = [], refetch } = useQuery({
-        queryKey: ['allUsers', ],
+        queryKey: ['allUsers',],
         queryFn: async () => {
             const res = await axiosSecure.get('/allUsers')
             return res.data
         }
     })
+    const [currentPage, setCurrentPage] = useState(0);
+    const itemsPerPage = 5;
+    const indexOfLastUser = (currentPage + 1) * itemsPerPage;
+    const indexOfFirstUser = indexOfLastUser - itemsPerPage;
+    const currentUsers = allUsers.slice(indexOfFirstUser, indexOfLastUser);
 
     const makeAdminHandle = allUser => {
         console.log(allUser._id)
@@ -52,14 +58,14 @@ const AllUser = () => {
 
     return (
         <div className="lg:min-h-screen">
-            <div className="text-center font-extrabold bg-[#F5AB35] text-5xl my-8">
-                <h1 className="p-5 ">All User : {allUsers.length}</h1>
+            <div className="text-center font-extrabold text-5xl my-8">
+                <h1>All User : {allUsers.length}</h1>
             </div>
             <div className="overflow-x-auto">
                 <table className="table table-xs  table-pin-rows table-pin-cols">
                     <thead className="">
-                        <tr className="text-sm  -z-10 text-[#222427]">
-                            <th>#</th>
+                        <tr className="text-sm -z-10 text-[#222427]">
+                            <th></th>
                             <td>User’s Name</td>
                             <td>Phone Number</td>
                             <td>Total Spent Amount</td>
@@ -69,19 +75,19 @@ const AllUser = () => {
                     </thead>
                     <tbody>
                         {
-                            allUsers?.map((allUser, index) => <tr key={allUser._id}>
-                                <th className="text-xl">{index + 1}</th>
-                                <td className="text-xl">{allUser.name}</td>
-                                <td className="text-xl">{allUser.phoneNumber}</td>
-                                <td className="text-xl">{allUser.requestedDeliveryDate}</td>
+                            currentUsers?.map((allUser, index) => <tr key={allUser._id}>
+                                <th>{index + 1}</th>
+                                <td>{allUser.name}</td>
+                                <td>{allUser.phoneNumber}</td>
+                                <td>{allUser.requestedDeliveryDate}</td>
 
 
-                                <th className="text-base">
+                                <th>
                                     {allUser.role === 'deliveryMen' ? 'Delivery Men' : <button
                                         onClick={() => makeDeliveryMan(allUser)}
                                         disabled={allUser.role === 'admin'} className="btn">Delivery Men</button>}
                                 </th>
-                                <th className="text-base">
+                                <th>
                                     {allUser.role === 'admin' ? 'Admin' : <button
                                         disabled={allUser.role === 'deliveryMen'}
                                         onClick={() => makeAdminHandle(allUser)}
@@ -97,7 +103,65 @@ const AllUser = () => {
                     </tbody>
 
                 </table>
-              
+                <nav className="flex items-center justify-center pt-20 -space-x-px">
+                    <button
+                        type="button"
+                        className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm first:rounded-s-lg last:rounded-e-lg border border-gray-200 text-gray-800 hover:bg-[#F5AB35] focus:outline-none focus:bg-[#F5AB35] disabled:opacity-50 disabled:pointer-events-none dark:border-gray-700 dark:hover:bg-white/10 dark:focus:bg-white/10"
+                        onClick={() => setCurrentPage((prev) => prev - 1)}
+                        disabled={currentPage === 0}
+                    >
+                        <svg
+                            className="flex-shrink-0 w-3.5 h-3.5"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width={24}
+                            height={24}
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth={2}
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        >
+                            <path d="m15 18-6-6 6-6" />
+                        </svg>
+                        <span className="hidden sm:block">Previous</span>
+                    </button>
+                    {[...Array(Math.ceil(allUsers.length / itemsPerPage)).keys()].map((page) => (
+                        <button
+                            key={page}
+                            type="button"
+                            className={`min-h-[38px] min-w-[38px] flex justify-center items-center ${currentPage === page
+                                    ? 'bg-gray-200 text-gray-800'
+                                    : 'border border-gray-200 text-gray-800 hover:bg-[#F5AB35]'
+                                } py-2 px-3 text-sm first:rounded-s-lg last:rounded-e-lg focus:outline-none focus:bg-gray-300 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-600 dark:border-gray-700 dark:text-white dark:focus:bg-[#F5AB35]`}
+                            onClick={() => setCurrentPage(page)}
+                        >
+                            {page + 1}
+                        </button>
+                    ))}
+                    <button
+                        type="button"
+                        className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm first:rounded-s-lg last:rounded-e-lg border border-gray-200 text-gray-800 hover:bg-[#F5AB35] focus:outline-none focus:bg-[#F5AB35] disabled:opacity-50 disabled:pointer-events-none dark:border-gray-700 dark:hover:bg-white/10 dark:focus:bg-white/10"
+                        onClick={() => setCurrentPage((prev) => prev + 1)}
+                        disabled={indexOfLastUser >= allUsers.length}
+                    >
+                        <span className="hidden sm:block">Next</span>
+                        <svg
+                            className="flex-shrink-0 w-3.5 h-3.5"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width={24}
+                            height={24}
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth={2}
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        >
+                            <path d="m9 18 6-6-6-6" />
+                        </svg>
+                    </button>
+                </nav>
             </div>
 
         </div>
